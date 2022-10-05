94fca3fee67fea028629f05efc29fda4f1bc3651
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-10-05 12:01:10 +0300

    ox12-javascript-warm_up task 3 commit

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+import { argv } from 'node:process';
+if (argv == NULL){
+  console.log('No argument');
+else if(argv == 1){
+  console.log('Argument found');
+else{
+  console.log('Arguments found');
